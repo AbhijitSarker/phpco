e0f48b2383b6e1a08ed808e2124c7543ba17c096
Abhijitsarker <abhijitsarker03@gmail.com>
2022-09-24 06:08:50 +0600

    const

---
+++ fundamentals/constant.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>constnt</title>
+</head>
+<body>
+     <?php 
+     define('pi', 3.14);
+     echo pi();
+
+     echo 'this is the circle radius value which is ','', 2*pi*5;
+    echo "<br>";
+     define('abc','this is abc');
+     echo abc;
+     echo "<br>";
+
+     const xyz = 'this is const keyword';
+     echo xyz;
+     ?>
+
+    
+</body>
+</html>
---
+++ fundamentals/variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <?php 
+        $name = 'abhijit';
+        $age = 21;
+        $weight = 60.5;
+        echo $name;
+        echo '<br>';
+        var_dump($age);
+
+    ?>    
+
+</body>
+</html>
