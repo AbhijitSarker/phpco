e313214ef524199527e13774155747d46061354e
Abhijitsarker <abhijitsarker03@gmail.com>
2022-09-24 06:36:37 +0600

    operator

---
+++ operator/operator.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operator</title>
+</head>
+<body>
+    <?php
+        $x=20;
+        $y=50;
+
+    
+        echo ($x+$y."<br>");
+        echo ($x-$y."<br>");
+        echo ($x*$y."<br>");
+        echo ($x/$y."<br>");
+        echo ($x%$y."<br>");
+        
+    ?>
+</body>
+</html>
