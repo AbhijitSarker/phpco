95352a9187e7b5e5aad4a7f23bfff5bf3f2cb98a
Abhijitsarker <abhijitsarker03@gmail.com>
2022-09-26 20:27:04 +0600

    regular commit

--- control/function.php
+++ control/function.php
@@ -25,10 +25,51 @@
 // echo $hello;
 
 
-function abc($num1, $num2){
-  $num3 = $num1 + $num2;
-  return $num3;
+// function abc($num1, $num2){
+//   $num3 = $num1 + $num2;
+//   return $num3;
+// }
+// $a = abc(9,10);
+// echo "the value after adding the function $a";
+
+// function abc($a="50"){
+//   echo "value $a \n";
+// }
+// abc();
+// abc(10);
+// abc(100);
+
+// function abc(){
+//   echo "this is dynamic functon call";
+// }
+
+// $func = "abc";
+// $func();
+
+// $a=function (){
+//   echo "this is a anonymous function";
+
+// };
+// $a();
+
+// function abc(){
+//   $a="php";
+//   echo $a;
+// }
+// abc();
+// echo $a;
+
+
+
+function fact($n){
+  if($n<0){
+    return -1;}
+  if($n==0){
+    return 1;}
+  return  ($n * fact($n-1));
 }
-$a = abc(9,10);
-echo "the value after adding the function $a";
+echo fact(5);
+
+
+
 ?>
