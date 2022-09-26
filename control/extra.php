e986e503732af4b1e31daed43bd17a3ae7e427ad
Abhijitsarker <abhijitsarker03@gmail.com>
2022-09-26 21:21:31 +0600

    regular commit

---
+++ control/extra.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>extra knowlwdge</h1>
+    <?php
+    // for($i=1; $i<10; $i++){
+    //     if($i==5){
+    //         continue;
+    //     }
+    //   echo $i;
+    //   echo "<br>";
+    // }
+
+
+    echo (abs(-7)."<br>"); 
+    echo (ceil(5.5)."<br>");
+    echo (ceil(5.3)."<br>");
+    echo (ceil(5.6)."<br>");
+
+
+
+    ?>
+</body>
+</html>
