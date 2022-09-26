07b289b56d18b7e76c72c685605696d02dceed4d
Abhijitsarker <abhijitsarker03@gmail.com>
2022-09-26 19:27:57 +0600

    regular commit

--- control/function.php
+++ control/function.php
@@ -1,12 +1,28 @@
 <?php
-function familyName($fname) {
-  echo "$fname Refsnes.<br>";
-}
+// function familyName($fname) {
+//   echo "$fname Refsnes.<br>";
+// }
 
-familyName("Jani");
-familyName("Hege");
-familyName("Stale");
-familyName("Kai Jim");
-familyName("Borge");
+// familyName("Jani");
+// familyName("Hege");
+// familyName("Stale");
+// familyName("Kai Jim");
+// familyName("Borge");
+
+function abc($i){
+
+  $i ="php";
+}
+$hi = "php5";
+abc($hi);
+echo $hi;
+
+function xyz(&$a){
+  $a="php";
+}
+$hello="this is php 5";
+xyz($hello);
+echo $hello;
+
 
 ?>
