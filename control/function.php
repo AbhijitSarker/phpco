a375d1eaff0f7fd49a4fbf57493d3b19ad767d46
Abhijitsarker <abhijitsarker03@gmail.com>
2022-09-30 19:22:25 +0600

    leap year

--- control/function.php
+++ control/function.php
@@ -61,15 +61,46 @@
 
 
 
-function fact($n){
-  if($n<0){
-    return -1;}
-  if($n==0){
-    return 1;}
-  return  ($n * fact($n-1));
-}
-echo fact(5);
+// function fact($n){
+//   if($n<0){
+//     return -1;}
+//   if($n==0){
+//     return 1;}
+//   return  ($n * fact($n-1));
+// }
+// echo fact(5);
 
+
+// PHP code to check if a given
+// year is leap year
+ 
+  function checkYear($year)
+    {
+      // If a year is multiple of 400,
+      // then it is a leap year
+      if ($year % 400 == 0)
+          print("Leap Year");
+          
+      // Else If a year is multiple of 100,
+      // then it is not a leap year
+      else if ($year % 100 == 0)
+          print("Not a Leap Year");
+              
+      // Else If a year is multiple of 4,
+      // then it is a leap year
+      else if ($year % 4 == 0)
+          print("Leap Year");
+          
+      else
+          print("Not a Leap Year");
+    }
+  
+  // Driver code
+  $year = 2003;
+  
+  checkYear($year);
+     
+// This code is contributed by ash264
 
 
 ?>
