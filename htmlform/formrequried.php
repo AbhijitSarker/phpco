42324755633788923e7084e71a1898cfd97502d3
Abhijitsarker <abhijitsarker03@gmail.com>
2022-10-01 04:41:11 +0600

    php added

--- htmlform/formrequried.php
+++ htmlform/formrequried.php
@@ -1,12 +1,42 @@
 <?php 
 
-    $nameErr = $emailErr  = $genderErr = $website = "";
+    $nameErr = $emailErr  = $genderErr = $websiteErr = "";
     $name = $email = $gender = $comment = $website = "";
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST["name"])){
+            $nameErr = "Name is required";
+        } else {
+            $name = test_input($_POST["name"]);
+        }
+
+        if(empty($_POST["email"])){
+            $emailErr = "Email is required";
+        } else{
+            $email = test_input($_POST["email"]);
+        }
+
+        if(empty($_POST["gender"])){
+            $genderErr = "Gender is required";
+        } else{
+            $gender = test_input($_POST["gender"]);
+        }
+
+        if(empty($_POST["website"])){
+            $websiteErr = "Gender is required";
+        } else{
+            $gender = test_input($_POST["gender"]);
+        }
         
     }
 
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
 ?>
 
 
